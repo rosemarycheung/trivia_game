44ac2d869f093e74458598ad58550f38fe40dd90
rosemarycheung <rosemaryrosec@gmail.com>
2019-07-26 16:56:36 -0700

    trivia game complete

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-questions = [
-    {
-        "question": "What is the largest organ?", 
-        "answer": "skin"
-    },
-    {
-        "question": "What planet do we live in?",
-        "answer": "Earth"
-    }
-]
+from clint.textui.colored import red, green, yellow, cyan, magenta
+import json
+
+
+def main():
+    """
+        Runs the trivia game
+    """
+    score = 0
+    
+    with open ("trivia_questions.json") as f:
+        questions = json.load(f)
+
+    name = input(yellow("What is your name?\n"))
+    print(magenta("Hello {}".format(name)))
+
+    for x in questions:
+        print(cyan("\n{}".format(x["question"])))
+        answer = input()
+        if answer.lower() == x["answer"].lower():
+            print(green("Correct!"))
+            score += 1
+        else:
+            print(red("Incorrect!"))
+    print(magenta("\nYour score: {}%".format(score/len(questions)*100)))
+
+if __name__ == "__main__":
+    main()
